7a8ca64ef2ee22e0c1a975b8e2fbd21f4a90ce13
Mike Morano <mmorano@mikeandwan.us>
2019-03-25 13:51:54 -0400

    split display of mobile v desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { DesktopViewModule } from './desktop-view/desktop-view.module';
+import { MobileViewModule } from './mobile-view/mobile-view.module';
 
 @NgModule({
     imports: [
         BrowserAnimationsModule,
         AppRoutingModule,
         CoreModule,
-        SharedModule
+        SharedModule,
+        DesktopViewModule,
+        MobileViewModule
     ],
     declarations: [
         AppComponent
