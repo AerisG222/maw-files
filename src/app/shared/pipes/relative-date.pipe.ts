bca54e2aaf00ad89dbe607477c572996bfa74797
Mike Morano <mmorano@mikeandwan.us>
2019-07-01 19:50:03 -0400

    adapt to changes in date-fns

--- src/app/shared/pipes/relative-date.pipe.ts
+++ src/app/shared/pipes/relative-date.pipe.ts
@@ -1,11 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { formatRelative } from 'date-fns';
+import { formatRelative, parseISO } from 'date-fns';
 
 @Pipe({
     name: 'relativeDate'
 })
 export class RelativeDatePipe implements PipeTransform {
     transform(value: any, args?: any): any {
+        if(typeof value === 'string') {
+            value = parseISO(value);
+        }
+
         return formatRelative(value, new Date());
     }
 }
