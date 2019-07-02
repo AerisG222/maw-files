1306a8edf186132228cc9ca0d98fb40555844f69
Mike Morano <mmorano@mikeandwan.us>
2019-07-01 20:48:05 -0400

    improve login page ui and functionality

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { OidcFacade } from 'ng-oidc-client';
+import { filter, tap, take } from 'rxjs/operators';
 
 @Component({
     selector: 'app-login',
@@ -7,12 +9,34 @@ import { OidcFacade } from 'ng-oidc-client';
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements AfterViewInit {
-
     constructor(
-        private oidcFacade: OidcFacade
-    ) { }
+        private oidcFacade: OidcFacade,
+        private router: Router
+    ) {
+
+    }
 
     ngAfterViewInit(): void {
+        this.oidcFacade.identity$.pipe(
+            tap(user => {
+                if (!!user && !user.expired) {
+                    this.onLoggedIn();
+                } else {
+                    this.popupLogin()
+                }
+            })
+        ).subscribe();
+    }
+
+    popupLogin(): void {
         this.oidcFacade.signinPopup();
+    }
+
+    redirectLogin(): void {
+        this.oidcFacade.signinRedirect();
+    }
+
+    onLoggedIn(): void {
+        this.router.navigate(['/']);
     }
 }
