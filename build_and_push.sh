4fc0a1810e60d1d9949ddd19f994bb0d045a97be
Mike Morano <mmorano@mikeandwan.us>
2020-05-24 06:56:45 -0400

    introduce github action

---
+++ build_and_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+IID_FILE=image.iid
+TAG=$(date +%Y%m%d%H%M%S)
+
+buildah bud -f Containerfile -t maw-files --iidfile "${IID_FILE}"
+
+IID=$(cat "${IID_FILE}")
+
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-files:"${TAG}"
