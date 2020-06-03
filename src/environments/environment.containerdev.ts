a8746773fc32cc3bd164185931f6e75b967587ff
Mike Morano <mmorano@mikeandwan.us>
2020-06-03 18:44:42 -0400

    separate dev from containerdev config

---
+++ src/environments/environment.containerdev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    version: require('../../package.json').version,
+
+    apiUrl: 'https://apidev.mikeandwan.us',
+    authUrl: 'https://authdev.mikeandwan.us',
+    filesUrl: 'http://filesdev.mikeandwan.us'
+};
