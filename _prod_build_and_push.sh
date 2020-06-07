b63cb17f4252770230a5176404c54f56c89a0946
Mike Morano <mmorano@mikeandwan.us>
2020-06-06 20:17:07 -0400

    push latest tag to docker

--- _prod_build_and_push.sh
+++ _prod_build_and_push.sh
@@ -3,7 +3,6 @@ IID_FILE=image.iid
 TAG=$(date +%Y%m%d%H%M%S)
 
 buildah bud -f Containerfile.prod -t maw-files --iidfile "${IID_FILE}"
-
 IID=$(cat "${IID_FILE}")
-
 buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-files:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-files:latest
