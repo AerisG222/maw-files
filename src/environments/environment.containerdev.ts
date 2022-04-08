43cf33ea3a58fb72510cddfc513cb4a4b2b2d549
Mike Morano <mmorano@mikeandwan.us>
2022-04-08 09:28:38 -0400

    improvements for test env:     - rename dev to test     - update ports     - update cert paths

--- src/environments/environment.containerdev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    version: require('../../package.json').version,
-
-    apiUrl: 'https://dev.api.mikeandwan.us',
-    authUrl: 'https://dev.auth.mikeandwan.us',
-    filesUrl: 'http://dev.files.mikeandwan.us'
-};
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    version: require('../../package.json').version,
+
+    apiUrl: 'https://test.api.mikeandwan.us:8443',
+    authUrl: 'https://test.auth.mikeandwan.us:8443',
+    filesUrl: 'https://test.files.mikeandwan.us:8443'
+};
